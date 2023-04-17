43f83af559aa59a4d29b7311203c13135824214e
lettdev <tungkradle@gmail.com>
2023-04-18 03:02:37 +0700

    Update the GetDesire func

--- TEST-mode_evasive_maneuvers .lua
+++ TEST-mode_evasive_maneuvers .lua
@@ -1,11 +1,31 @@
 function GetDesire()
 
+local npcBot = GetBot();
 
-return (0.0)
+local fBestDefendScore = 0;
+local npcBestDefendableAlly = nil;
+local npcBestAttackingEnemy = nil;
+
+local tableNearbyRetreatingAlliedHeroes = npcBot:GetNearbyHeroes( 1000, false, BOT_MODE_RETREAT );
+if ( #tableNearbyRetreatingAlliedHeroes == 0 )
+then
+return BOT_MODE_DESIRE_NONE;
 end
 
+local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( 1000, true, BOT_MODE_NONE );
 
-function Think()
-	
-	
+-- Is there a good enemy to attack, who's scary to our ally and not too scary to us?
+for _,npcAlly in pairs( tableNearbyRetreatingAlliedHeroes )
+do
+local fDefendScore, npcEnemy = GetDefendScore( npcAlly, tableNearbyEnemyHeroes );
+
+if ( fDefendScore > fBestDefendScore )
+then
+fBestDefendScore = fDefendScore;
+npcBestDefendableAlly = npcAlly;
+npcBestAttackingEnemy = npcEnemy;
+end
+end
+
+return RemapValClamped( fBestDefendScore, 0.9, 1.0, BOT_MODE_DESIRE_NONE, BOT_MODE_DESIRE_HIGH );
 end
